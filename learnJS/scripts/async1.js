13053d45e70543fefc9bdfef632a9d67a0001b80
Bravechen <brave_cgx@163.com>
2017-11-05 09:48:41 +0800

    添加异步脚本执行的例子

---
+++ learnJS/scripts/async1.js
@@ -0,0 +1,8 @@
+(function(){
+  var a = 100000000;
+  var m = 0;
+  for(var i=0;i<a;i++){
+    m++;
+  }
+  console.log("async1 exec complete.",m,i);
+})();
--- learnJS/scripts/index1.js
+++ learnJS/scripts/index1.js
@@ -1,37 +1,13 @@
 (function(){
 
-  function mixin(){
-    if(arguments.length===0){
-      return;
-    }
+  var doc = window.documentElement || window.document;
 
-    var args = Array.prototype.slice.call(arguments,0),
-        useDeep = args[0],
-        target,
-        from,
-        froms;
-    // = Array.prototype.slice.call(arguments,0)
-    if(typeof useDeep === 'boolean'){
-      
-      target = args[1] || {}; 
-      froms = args.slice(2);
-    }else{
-      target = useDeep || {};
-      froms = args.slice(1);
-    }
+  doc.addEventListener('DOMContentLoaded',function(e){
+    console.log("DOMContentLoaded fired.",e);
+  });
 
-    var len = froms.length;
-    for(var i=0;i<len;i++){
-      from = froms[i];
-      if(!from || from === target){
-        continue;
-      }
-      for(var key in from){
-        
-      }
-    }
-
-    return target;
-  }
+  window.addEventListener('load',function(e){
+    console.log("window load fired",e);
+  });
 
 })();
